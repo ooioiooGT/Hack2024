f9b588549a940526ff966bec4b65688bd4ed1e80
lingarner <97457456+lingarner@users.noreply.github.com>
2024-03-02 08:27:48 -0700

    profile skeleton

--- Screen/Profile.js
+++ Screen/Profile.js
@@ -1,13 +1,30 @@
-import { StyleSheet, View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text, Image, TextInput, Button} from 'react-native'
+import React, {useState} from 'react'
 import { BlurView } from '@react-native-community/blur';
 import Nav from '../components/Nav';
 
 
 const Options = () => {
+  const [firstName, setFirstName] = useState('John');
+  const [lastName, setLastName] = useState('Doe');
+  const [email, setEmail] = useState('Johnny@gmail.com');
+  const [password, resetPassword] = useState('***********');
+
+
   return (
     <View style={styles.container}>
-        <Text>PROFILE</Text>
+        <Image source={require('../assets/puppy.webp')} style={styles.img}/>
+        <View>
+          <TextInput style={styles.input} placeholder='First Name' value={firstName} onChangeText={(text)=>setFirstName(text)}/>
+          <TextInput style={styles.input} placeholder='Last Name' value={lastName} onChangeText={(text)=>setLastName(text)}/>
+          <TextInput style={styles.input} placeholder='Email' value={email} onChangeText={(text)=>setEmail(text)}/>
+          <TextInput style={styles.input} placeholder='Password' value={password} onChangeText={(text)=>resetPassword(text)}/>
+          </View>
+
+          <View style={styles.org}>
+            <Button color='#FD8450' title='Update'/>
+            <Button color='#FD8450' title='Logout'/>
+        </View>
         <Nav/>
     </View>
   )
@@ -18,6 +35,44 @@ export default Options
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#302D43'
+    backgroundColor: '#302D43',
+    justifyContent: 'center',
+    paddingHorizontal: 20, 
+
   },
+  color:{
+    color: '#FFF',
+  },
+  input:{
+    marginHorizontal: 20,
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    top: -30,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+},
+img:{
+  width: 200, 
+  height: 200, 
+  borderRadius: 100, 
+  alignItems: 'center', 
+  alignSelf: 'center', 
+  top:-70,
+  borderWidth: 3,
+  borderColor: '#8930E8'
+},
+
+org:{
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  color: '#FFF',
+}
 })
