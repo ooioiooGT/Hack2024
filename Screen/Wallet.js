9cad200e2e22d0a7c7dd4fae11209b094be05ec3
lingarner <97457456+lingarner@users.noreply.github.com>
2024-03-02 07:02:55 -0700

    wallet nav added

--- Screen/Wallet.js
+++ Screen/Wallet.js
@@ -1,12 +1,14 @@
 import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
 import { BlurView } from '@react-native-community/blur';
+import Nav from '../components/Nav';
 
 
 const Wallet = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>WALLET</Text>
+        <Nav/>
     </View>
   )
 }
@@ -14,6 +16,8 @@ const Wallet = () => {
 export default Wallet
 
 const styles = StyleSheet.create({
-  
-
+  container: {
+    flex: 1,
+    backgroundColor: '#302D43'
+  },
 })
