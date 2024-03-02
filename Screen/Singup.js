c5bd6765a13863ff45b495043c3bc011ce93c8c3
jed0203 <cha19057@byui.edu>
2024-03-01 18:49:58 -0700

    adding singup function

--- Screen/Singup.js
+++ Screen/Singup.js
@@ -1,7 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {createUserWithEmailAndPassword} from 'firebase/auth';
 
 const Singup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = FIREBASE_AUTH;
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+        const response = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(response);
+    } catch (error){
+        console.log(error);
+        alert('Sign in failed: ' + error.message);
+    } finally {
+        setLoading(false);
+    }
+  }
+
   return (
     <View>
       <Text>Singup</Text>
