2702ad6b64d091344acde2e5ca6b82a19f6ab8c9
lingarner <97457456+lingarner@users.noreply.github.com>
2024-03-02 11:10:30 -0700

    imports not being used

--- Screen/Home.js
+++ Screen/Home.js
@@ -1,10 +1,7 @@
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { BlurView } from '@react-native-community/blur';
 import { useNavigation } from '@react-navigation/native';
 import Nav from '../components/Nav';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHouse, faWallet, faChartSimple, faUser} from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   const navigation = useNavigation();
