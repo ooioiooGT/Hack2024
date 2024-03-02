01415e69151fc7394d2a04cc5aba89186db94a9e
lingarner <97457456+lingarner@users.noreply.github.com>
2024-03-01 22:49:19 -0700

    login test

--- Screen/Login.js
+++ Screen/Login.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Singup from './Signup';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { Auth } from '../Firebase';
+import { LinearGradient } from 'react-native-linear-gradient';
 
 const Login = () => {
   const navigation = useNavigation();
@@ -16,14 +17,19 @@ const Login = () => {
   };
   }
   return (
-    <View>
-      <TextInput placeholder='email' />
-      <TextInput placeholder='password' secureTextEntry={true}/>
+    <View style={styles.container}>
+      <Text style={styles.h1}>CashIQ</Text>
+      <TextInput style={styles.input} placeholder='email' />
+      <TextInput style={styles.input} placeholder='password' secureTextEntry={true}/>
+
       <TouchableOpacity>
-        <Text onPress={login}>Login</Text>
+        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']}>
+          <Text onPress={login} style={styles.loginBtn}>Login</Text>
+        </LinearGradient>
       </TouchableOpacity>
+
       <TouchableOpacity onPress={() => navigation.navigate('Singup')}>
-        <Text>Singup</Text>
+        <Text>Sign-Up</Text>
       </TouchableOpacity>
     </View>
   )
@@ -31,4 +37,38 @@ const Login = () => {
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  h1:{
+    color: '#fff',
+    fontSize: 30
+  }, 
+
+  input:{
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: '#fff',
+    width: '80%'
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#302D43',
+  },
+
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#5F5F5F',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  }
+})
