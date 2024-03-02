8e4ff06e4554270cb713a6953083fa4377ca4281
Gilber Chen <gilber1002@byui.edu>
2024-03-01 17:55:15 -0700

    install firebase

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>pen up App.js to start working on your apOp!</Text>
       <StatusBar style="auto" />
     </View>
   );
